f64a6362206462e8dbf614ee1c1010fd44b16882
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-05-14 17:32:01 -0300

    Configurando o Knex.js
    Nesta aula, foi feita a configuração do Knex em uma aplicação.

---
+++ kenxfile.ts
@@ -0,0 +1,13 @@
+export default{
+    client: 'sqlite3',
+    connection: {
+        filename: './src/database/database.db'
+    },
+
+    useNullAsDefault: true,
+
+    migrations: {
+        extensions: ['ts'],
+        directory: './src/database/migrations'
+    },
+}
