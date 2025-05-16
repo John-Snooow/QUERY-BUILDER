e531bb35bdc6bc80cf7ce38190b5b10e06eeec07
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-05-16 20:14:58 -0300

    Projeto concluído
    Concluído porém com alguns erros que devem ser corrigidos

--- kenxfile.ts
+++ kenxfile.ts
@@ -1,13 +1,21 @@
-export default{
+module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: './src/database/database.db'
+      filename: "./src/database/database.db"
     },
-
+    pool: {
+      afterCreate: (connection: any, done: any) => {
+        connection.run("PRAGMA foreign_keys = ON")
+        done()
+      }
+    },
     useNullAsDefault: true,
-
     migrations: {
-        extensions: ['ts'],
-        directory: './src/database/migrations'
+      extensions: "ts",
+      directory: "./src/database/migrations"
     },
-}
+    seeds: {
+      extensions: "ts",
+      directory: "./src/database/seeds"
+    }
+  }
